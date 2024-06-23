02f43962d9f3365511da0095c8c9298c546fbef8
= <=>
2024-06-23 23:50:29 +0400

    add get list data

--- books/schema4.py
+++ books/schema4.py
@@ -19,9 +19,17 @@ class Query(graphene.ObjectType):
     all_books = graphene.Field(BookType, id=graphene.Int())  # single object data can be retrieved this way,
     # graphene.Field method states that a single field is used,
     # graphene.Int() method specifies the type of field to use
+    all_book_libraries = graphene.List(LibraryType, book_id=graphene.Int())
 
     def resolve_all_books(root, info, id):
         return Book.objects.get(pk=id)
+
+    def resolve_all_book_libraries(root, info, book_id):
+        try:
+            book = Book.objects.get(pk=book_id)
+            return book.library.all()
+        except Book.DoesNotExist:
+            return None
 
 
 schema = graphene.Schema(query=Query)
